09a0544855e19d19f06dc86f14b650093057b52c
edelmutic <edelmutic@gmail.com>
2021-05-16 19:24:21 +0300

    update server

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ dotenv.config();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+const corsOptions = { credentials: true, origin: process.env.URL || '*' };
 
 app.get('/', (req, res) => {
   res.send('Hello world');
